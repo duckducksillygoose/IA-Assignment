43288237f6bc536b175339c8aa0254da76ccdae6
duckducksillygoose <Ashleigh.Gurr@student.curtin.edu.au>
2026-03-17 09:56:53 +0800

    Added a list of nodes to make printing easier as well as an addition method. Will do subtraction if time. AG

--- IAGraph.py
+++ IAGraph.py
@@ -1,8 +1,12 @@
 from LinkedListsetup import LinkedList, ListNode
 
+
+
+
 class IAGraph():
     def __init__(self, size):
         self.size = size
+        self.nodelist = LinkedList()
 
 class GraphNode():
     def __init__(self, name, h):
@@ -13,7 +17,17 @@ class GraphNode():
         self.f = float("inf") #total
 
 
+    def add_node(self, name):
+        node = GraphNode(name)
+        self.nodelist.InsertNode(node)
+
+
 class GraphEdge():
     def __init__(self, n, weight):
         self.n= n
         self.weight = weight
+
+
+
+
+
