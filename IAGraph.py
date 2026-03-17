f05b312071fe94c04bdc151b14d3a156d311c229
duckducksillygoose <Ashleigh.Gurr@student.curtin.edu.au>
2026-03-17 09:53:12 +0800

    Created the base structure for a graph with nodes and edges, as well as a containing structure. AG

--- IAGraph.py
+++ IAGraph.py
@@ -1,1 +1,19 @@
 from LinkedListsetup import LinkedList, ListNode
+
+class IAGraph():
+    def __init__(self, size):
+        self.size = size
+
+class GraphNode():
+    def __init__(self, name, h):
+        self.name = name
+        self.neighbours = LinkedList()
+        self.h = h #heuristic cost from cell to target
+        self.g = float("inf") #cost from start to this cell
+        self.f = float("inf") #total
+
+
+class GraphEdge():
+    def __init__(self, n, weight):
+        self.n= n
+        self.weight = weight
