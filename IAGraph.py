c940d42d093144542a33bc986a4ddcbd7ce02e03
duckducksillygoose <Ashleigh.Gurr@student.curtin.edu.au>
2026-03-17 10:17:49 +0800

    Successfully moved cities to a different file and printed. AG

--- IAGraph.py
+++ IAGraph.py
@@ -77,21 +77,7 @@ class GraphEdge():
         self.weight = weight
 
 
-testgraph = IAGraph(8)
-testgraph.add_node("Perth", 3400)
-testgraph.add_node("Adelaide", 2200)
-testgraph.add_node("Melbourne", 2300)
-testgraph.add_node("canberra", 2100)
-testgraph.add_node("sydney", 1950)
-testgraph.add_node("Newcastle", 1850)
-testgraph.add_node("Brisbane", 1400)
-testgraph.add_node("Gold Coast", 1500)
-testgraph.add_node("Hobart", 2600)
-testgraph.add_node("Darwin", 1700)
-testgraph.add_node("Alice Springs", 1500)
-testgraph.add_node("Townsville", 280)
-testgraph.add_node("Cairns", 0)
-testgraph.display_as_list()
+
 
 
 
