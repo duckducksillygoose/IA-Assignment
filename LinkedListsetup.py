93b0df32c490f41021791a4426be99e3da230a2d
duckducksillygoose <Ashleigh.Gurr@student.curtin.edu.au>
2026-03-17 09:41:25 +0800

    Wrote a quick linkedlist and insert node setup, as well as the listnode class for adding. AG

---
+++ LinkedListsetup.py
@@ -0,0 +1,20 @@
+class LinkedList():
+    def __init__(self):
+        self.head = None
+
+    def InsertNode(self, element):
+        newNode = ListNode(element) 
+        if self.head is None:
+            self.head = newNode
+            return
+        current_node = self.head
+        while current_node.next:
+            current_node = current_node.next
+        current_node.next = newNode
+
+
+class ListNode:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
