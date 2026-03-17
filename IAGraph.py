414ed6099b45e744392a2cd4fc8c0cc7d1255e17
duckducksillygoose <Ashleigh.Gurr@student.curtin.edu.au>
2026-03-17 10:00:31 +0800

    Wrote a method to add a road between cities, will be creating a test file for this. The next goal after this is printing so I can debug. AG

--- IAGraph.py
+++ IAGraph.py
@@ -22,6 +22,29 @@ class GraphNode():
         self.nodelist.InsertNode(node)
 
 
+    def add_road(self, city1, city2, weight):
+        city1 = city2 = None
+
+        current = self.nodelist.head
+        while current:
+            if current.data.data == city1:
+                city1 = current.data
+            if current.data.data == city1:
+                
+                city2 = current.data
+            current = current.next
+
+        if city1 is None or city2 is None:
+            raise Exception("One or both nodes not found") #check against misspellings
+        
+
+
+        city1.neighbours.InsertNode(GraphEdge(city2, weight)) #bidirectional
+        city2.neighbours.InsertNode(GraphEdge(city1, weight))
+
+  
+
+
 class GraphEdge():
     def __init__(self, n, weight):
         self.n= n
