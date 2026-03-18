c1b52456d59aee3997f4395c6904e83f8d6acea9
duckducksillygoose <Ashleigh.Gurr@student.curtin.edu.au>
2026-03-18 12:01:53 +0800

    Wrote a function to locate the cities, trying to test with print functions, has succeeded. Next step is to create the pathfinding algorithm. AG

--- astarsearch.py
+++ astarsearch.py
@@ -4,4 +4,20 @@
 from IAGraph import *
 from LinkedListsetup import *
 
+def astar(graph, start_city, goal_city):
+    # Find cairns and perth
+    current = graph.nodelist.head
+    start_node = goal_node = None
+    while current:
+        if current.data.name == start_city:
+            start_node = current.data
+        if current.data.name == goal_city:
+            goal_node = current.data
+        current = current.next
+
+    if start_node is None or goal_node is None:
+        raise Exception("Start or goal node not found!")
+    else:
+        print("Nodes found")
+    
 
