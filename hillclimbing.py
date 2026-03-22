5f5833bb27d5fec9bc22ef0a03d1a8c97b139f82
duckducksillygoose <Ashleigh.Gurr@student.curtin.edu.au>
2026-03-22 12:30:08 +0800

    Working on hillclimbing alg. AG

--- hillclimbing.py
+++ hillclimbing.py
@@ -19,3 +19,23 @@ def hill_climb(graph, start_city, goal_city):
         raise Exception("Start or goal node not found!")
     else:
         print("Nodes found") 
+
+    path = []
+
+    current_node = start_node
+
+
+    while True:
+        path.append(current_node.name)
+
+        if current_node.name == goal_node.name:
+            print("Goal city has been reached")
+            return path
+        
+        best_neighbour = None
+        best_h = current_node.h
+
+        neighbour_current = current_node.neighbours.head
+
+        while neighbour_current:
+            neighbour = neighbour_current.data.n #GraphEdge object
