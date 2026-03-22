c1ba07e63eb514c060d13cd5affde899ead9a510
duckducksillygoose <Ashleigh.Gurr@student.curtin.edu.au>
2026-03-22 12:20:46 +0800

    Changed up my variable names to match the previous code. AG

--- hillclimbing.py
+++ hillclimbing.py
@@ -3,7 +3,7 @@ from cities import *
 
 #hill climbing algorithm
 
-def hill_climb(graph, start_node, end_node):
+def hill_climb(graph, start_city, goal_city):
         
     # Find cairns and perth
     current = graph.nodelist.head
@@ -11,11 +11,11 @@ def hill_climb(graph, start_node, end_node):
     while current:
         if current.data.name == start_city:
             start_node = current.data
-        if current.data.name == goal_city:
+        if current.data.name == goal_city: #copied over code from a star search
             goal_node = current.data
         current = current.next
 
     if start_node is None or goal_node is None:
         raise Exception("Start or goal node not found!")
     else:
-        print("Nodes found") #copied over code from a star search
+        print("Nodes found") 
