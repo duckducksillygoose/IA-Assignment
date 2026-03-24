75ed3446c9e97938d10f6dc62c2088b28e8c1a81
duckducksillygoose <Ashleigh.Gurr@student.curtin.edu.au>
2026-03-24 11:35:57 +0800

    Comments on my algorithm I need to follow when coding. AG

--- greedy.py
+++ greedy.py
@@ -3,3 +3,9 @@
 from LinkedListsetup import *
 from cities import *
 from IAGraph import *
+
+#notes for doing this algorithm
+#find the start and end nodes
+# find neighbours and calculate the heuristic, compare all neighbours and find the lowest heuristic
+#choose that neighbour and repeat
+#different to hill climbing that f=h, also uses manhattan distance so have to code that
