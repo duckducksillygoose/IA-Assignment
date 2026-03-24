e5ce52b75fbab7d7ca9e56707893a4b5b8b484b2
duckducksillygoose <Ashleigh.Gurr@student.curtin.edu.au>
2026-03-24 11:41:58 +0800

    Copied some code from astar for node finding. AG

--- greedy.py
+++ greedy.py
@@ -3,9 +3,33 @@
 from LinkedListsetup import *
 from cities import *
 from IAGraph import *
+from astarsearch import reconstruct_path
 
 #notes for doing this algorithm
 #find the start and end nodes
 # find neighbours and calculate the heuristic, compare all neighbours and find the lowest heuristic
 #choose that neighbour and repeat
 #different to hill climbing that f=h, also uses manhattan distance so have to code that
+
+
+
+def greedy_BFS(graph, start_city, end_city): #copied from astar
+        # Find cairns and perth
+    current = graph.nodelist.head
+    start_node = goal_node = None
+    while current:
+        if current.data.name == start_city:
+            start_node = current.data
+        if current.data.name == goal_city:
+            goal_node = current.data
+        current = current.next
+
+    if start_node is None or goal_node is None:
+        raise Exception("Start or goal node not found!")
+    else:
+        print("Nodes found")
+
+
+    current = graph.nodelist.head
+    open_set = [start_node]
+    closed_set = []
