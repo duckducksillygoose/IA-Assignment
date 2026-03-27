3181d3fc1e31bb39bb70608b409bfecfa599ce99
duckducksillygoose <Ashleigh.Gurr@student.curtin.edu.au>
2026-03-27 11:09:25 +0800

    put the networkx rep into a different file by renaming main.py. AG

--- graph_rep.py
+++ graph_rep.py
@@ -8,6 +8,7 @@ import time
 
 nx_graph =testgraph.convert_to_nx()
 print(type(nx_graph))
+plt.show()
 
 
 #nx.draw(nx_graph, with_labels=True, node_size=1000, node_color='skyblue', font_size=12, font_weight='bold', edge_color='blue')
@@ -23,7 +24,6 @@ print(type(nx_graph))
 #hillclimb(testgraph, "Perth", "Cairns")
 #time.sleep(2)
 
-astar(testgraph, "Perth", "Cairns")
 
 #print("____GREEDY BEST FIRST SEARCH RESULTS______")
 #greedy_BFS(testgraph, "Perth", "Cairns")
